875a239c7f1c902451c7ab74fa451c7ba81e67bd
Laurent Savaete <laurent@where.tf>
2020-12-30 20:23:28 +0000

    Try one more idea to get ssh keys loaded

--- set_env_vars_based_on_git_branch.sh
+++ set_env_vars_based_on_git_branch.sh
@@ -8,11 +8,11 @@
 if [ "${CI_COMMIT_BRANCH}" == "master" ]; then
     echo "Running on master"
     export DEPLOY_SERVER_NAME="production"
-    echo $SSH_PRIVKEY_PRODUCTION | tr -d '\r' | ssh-add -
+    echo "${SSH_PRIVKEY_PRODUCTION}" | tr -d '\r' | ssh-add -
 elif [ "${CI_COMMIT_BRANCH}" == "staging" ]; then
     echo "Running on staging"
     export DEPLOY_SERVER_NAME="staging"
-    echo $SSH_PRIVKEY_STAGING | tr -d '\r' | ssh-add -
+    echo "${SSH_PRIVKEY_STAGING}" | tr -d '\r' | ssh-add -
 else
     echo "Running on a non-deploying branch"
 fi
