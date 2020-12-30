26ca9038d657acaf3d0b578f70ebbaa5bfccf39e
Laurent Savaete <laurent@where.tf>
2020-12-30 19:56:09 +0000

    Use gitlab-ci env var name

--- set_env_vars_based_on_git_branch.sh
+++ set_env_vars_based_on_git_branch.sh
@@ -5,11 +5,11 @@
 # are a bit buggy for our use case
 # (https://github.com/travis-ci/travis-ci/issues/4681)
 
-if [ "${TRAVIS_BRANCH}" == "master" ]; then
+if [ "${CI_COMMIT_BRANCH}" == "master" ]; then
     echo "Running on master"
     export DEPLOY_SERVER_NAME="production"
     echo "$SSH_PRIVKEY_PRODUCTION" | tr -d '\r' | ssh-add -
-elif [ "${TRAVIS_BRANCH}" == "staging" ]; then
+elif [ "${CI_COMMIT_BRANCH}" == "staging" ]; then
     echo "Running on staging"
     export DEPLOY_SERVER_NAME="staging"
     echo "$SSH_PRIVKEY_STAGING" | tr -d '\r' | ssh-add -
