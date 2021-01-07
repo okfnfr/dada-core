a82b73c38f0941c720bb81af40a81525142e888f
Laurent Savaete <laurent@where.tf>
2021-01-07 10:49:41 +0000

    Try yet another option for ssh keys

--- set_env_vars_based_on_git_branch.sh
+++ set_env_vars_based_on_git_branch.sh
@@ -13,8 +13,8 @@ if [ "${CI_COMMIT_BRANCH}" == "master" ]; then
 elif [ "${CI_COMMIT_BRANCH}" == "staging" ]; then
     echo "Running on staging"
     export DEPLOY_SERVER_NAME="staging"
-    #echo "${SSH_PRIVKEY_STAGING}" | tr -d '\r' | ssh-add -
-    ssh-add $SSH_PRIVKEY_STAGING
+    echo "$SSH_PRIVKEY_STAGING" | tr -d '\r' | ssh-add -
+    #ssh-add $SSH_PRIVKEY_STAGING
     echo "ssh keys loaded"
 else
     echo "Running on a non-deploying branch"
