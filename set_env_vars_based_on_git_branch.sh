32b0d572aefbba56840119c14171fcbd6bf98b33
Laurent Savaete <laurent@where.tf>
2021-01-07 16:21:49 +0000

    Cleanup CI script

--- set_env_vars_based_on_git_branch.sh
+++ set_env_vars_based_on_git_branch.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# check what git branch we're on, and export a few vars
-# this saves us from using travis' matrix builds, which
-# are a bit buggy for our use case
-# (https://github.com/travis-ci/travis-ci/issues/4681)
+# decide which SSH key and variables to use based on
+# the current branch.
+# In gitlab CI, the variables can be protected to only be
+# available in protected branches, so check settings if
+# the value is not accessible here
 
 if [ "${CI_COMMIT_BRANCH}" == "master" ]; then
     echo "Running on master"
     export DEPLOY_SERVER_NAME="production"
-    echo "${SSH_PRIVKEY_PRODUCTION}" | tr -d '\r' | ssh-add -
-    echo "ssh keys loaded"
+    echo "$SSH_PRIVKEY_PRODUCTION" | tr -d '\r' | ssh-add -
 elif [ "${CI_COMMIT_BRANCH}" == "staging" ]; then
     echo "Running on staging"
     export DEPLOY_SERVER_NAME="staging"
     echo "$SSH_PRIVKEY_STAGING" | tr -d '\r' | ssh-add -
-    #ssh-add $SSH_PRIVKEY_STAGING
-    echo "ssh keys loaded"
 else
     echo "Running on a non-deploying branch"
 fi
